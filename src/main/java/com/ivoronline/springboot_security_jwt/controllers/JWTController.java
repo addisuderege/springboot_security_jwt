eb9397c3b17541def2a1b605385ca5c82d19f546
igoreta <ivoronline@gmail.com>
2021-02-01 10:22:07 +0100

    Commit

---
+++ src/main/java/com/ivoronline/springboot_security_jwt/controllers/JWTController.java
@@ -0,0 +1,19 @@
+package com.ivoronline.springboot_security_jwt.controllers;
+
+import com.ivoronline.springboot_security_jwt.config.JWTUtil;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class JWTController {
+
+  @ResponseBody
+  @RequestMapping("/CreateJWT")
+  public String createJWT() {
+    String jwt = JWTUtil.createJWT("1", "ivoronline", "TestJWT");
+    return jwt;
+  }
+
+
+}
