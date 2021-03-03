aa6a9daa7876240b1f9ba4af4e8c4e50f6a38b08
igoreta <ivoronline@gmail.com>
2021-03-03 14:01:22 +0100

    Commit

--- src/main/java/com/ivoronline/springboot_security_jwt/config/MyAuthenticationManager.java
+++ src/main/java/com/ivoronline/springboot_security_jwt/config/MyAuthenticationManager.java
@@ -15,26 +15,30 @@ public class MyAuthenticationManager implements AuthenticationManager {
     @Override
     public Authentication authenticate(Authentication enteredAuthentication) {
 
-      //HARD CODED USER
-      //Here you would call UserDetailsService(enteredUsername) to get UserDetails with Password & Authorities.
-      String username = "myuser";
-      String password = "mypassword";
-      String role     = "ROLE_USER";
-
       //GET ENTERED CREDENTIALS
-      String enteredUsername = (String) enteredAuthentication.getPrincipal();   //USERNAME
-      String enteredPassword = (String) enteredAuthentication.getCredentials(); //PASSWORD
+      String enteredUsername = (String) enteredAuthentication.getPrincipal();   //ENTERED USERNAME
+      String enteredPassword = (String) enteredAuthentication.getCredentials(); //ENTERED PASSWORD
 
-      //AUTHENTICATE USER
-      if (!enteredUsername.equals(username)) { System.out.println("Username not found"); return null; }
-      if (!enteredPassword.equals(password)) { System.out.println("Incorrect Password"); return null; }
+      //GET STORED CREDENTIALS
+      //Here they are hard coded (for simplicity reason)
+      //Call UserDetailsService(enteredUsername) to get UserDetails with Password & Authorities from DB
+      String storedUsername    = "admin";
+      String storedPassword    = "adminpassword";
+      String storedAuthorities = "book.read, book.delete";
+
+      //AUTHENTICATE USER (COMPARE ENTERED AND STORED CREDENTIALS)
+      if (!enteredUsername.equals(storedUsername)) { System.out.println("Username not found"); return null; }
+      if (!enteredPassword.equals(storedPassword)) { System.out.println("Incorrect Password"); return null; }
 
       //CREATE AUTHORITIES
+      String[] authoritiesArray = storedAuthorities.split(", ");
       List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
-                             authorities.add(new SimpleGrantedAuthority(role));
+      for(String authority : authoritiesArray) {
+        authorities.add(new SimpleGrantedAuthority(authority));
+      }
 
       //CREATE VALIDATED AUTHENTICATION
-      Authentication validatedAuth = new UsernamePasswordAuthenticationToken(username,password,authorities);
+      Authentication validatedAuth = new UsernamePasswordAuthenticationToken(enteredUsername, null, authorities);
 
       //RETURN VALIDATES AUTHENTICATION
       return validatedAuth;
--- src/main/java/com/ivoronline/springboot_security_jwt/controllers/MyController.java
+++ src/main/java/com/ivoronline/springboot_security_jwt/controllers/MyController.java
@@ -1,5 +1,6 @@
 package com.ivoronline.springboot_security_jwt.controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,9 +9,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class MyController {
 
   @ResponseBody
-  @RequestMapping("/Hello")
-  public String hello() {
-    return "Hello from Controller";
+  @PreAuthorize("hasAuthority('book.create')")
+  @RequestMapping("/CreateBook")
+  public String createBook() {
+    return "Only ADMIN can create Book";
   }
 
 }
